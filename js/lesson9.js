97ac35070053cca5be1567b06567ac6b51d9ded0
Oleksandra <157833785+dulidawi@users.noreply.github.com>
2025-09-06 12:46:02 +0200

    123

---
+++ js/lesson9.js
@@ -0,0 +1,18 @@
+// function logItems(array) {
+//     for (let items = 0; items < logItems.length; items += 1) {         
+        
+//     }
+// }
+
+// console.log(logItems(['Apple', 'Banana', 'Grape', 'Pineapple']));
+
+
+// Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію calculateEngravingPrice(message, pricePerWord) приймаючу рядок (в рядку будуть тільки слова і прогалини) і ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
+
+function calculateEngravingPrice(message, pricePerWord) {
+    let price = 50
+    
+    
+}
+
+console.log(calculateEngravingPrice("I love you, sweetheart!"));
